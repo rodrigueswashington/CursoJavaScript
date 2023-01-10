c858e59de619ac53206a1b148f42ccdd4a85f5a6
Washington Rodrigues <washington_sr_@hotmail.com>
2023-01-10 00:12:03 -0300

    Aulas 13 e 14 - estruturas de repetição

---
+++ Aula013/ambiente.js
@@ -0,0 +1,13 @@
+var contador = 1
+while (contador <= 3){
+    console.log(`Passo ${contador}`)
+    contador++ //++ indica que a variação recebe o valor dela + 1
+}
+
+console.log('---------------------------------------------------')
+
+var cont = 1
+do{
+    console.log(`Passo ${cont}`)
+    cont++
+} while(cont <= 6)
---
+++ Aula014/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai começar...')
+for (var cont = 1; cont <= 5; cont++){
+    console.log(`Passo ${cont}`)
+}
+console.log('Fim!')
