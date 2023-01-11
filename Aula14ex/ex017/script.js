76fd913602d451bc2c741e09f80247fc21a5af19
Washington Rodrigues <washington_sr_@hotmail.com>
2023-01-10 21:38:18 -0300

    Fiz o ex017 - tabuada

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -1,2 +1,19 @@
-
-
+function calcular(){
+    let número = document.getElementById('txtn')
+    let tabuada = document.getElementById('seltab')
+    
+    if (número.value.length == 0){
+        window.alert('Por favor, digite um número!')
+    } else{
+        let num = Number(número.value)
+        let cont = 1
+        tabuada.innerHTML = ''
+        while (cont <= 10){
+            let item = document.createElement('option')
+            item.text = `${num} x ${cont} = ${num * cont}`
+            item.value = `tabuada${cont}`
+            tabuada.appendChild(item)
+            cont++
+        }   
+    }
+}
